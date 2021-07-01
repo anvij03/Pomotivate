925dd654053f554902ccb812a72026caba7bdc34
Freya Frahm <freyafrahm@gmail.com>
2021-07-01 10:31:17 -0700

    fixed break time view controller, formula is not coded yet

--- Pomotivate/breakViewController.swift
+++ Pomotivate/breakViewController.swift
@@ -1,129 +1,39 @@
+//
+//  BreakViewController.swift
+//  Pomotivate
+//
+//  Created by  Scholar on 7/1/21.
+//
+
 import UIKit
 
+class BreakViewController: UIViewController {
 
-
-
-
-
-
-
-/*
-var selectedDateandTime = Date()
-
-class weeklyViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDelegate, UITableViewDataSource
-    
-{
-    
-    @IBOutlet weak var monthLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    @IBOutlet weak var backgroundWeeklyView: UIView!
-    
-    var totalSquares = [Date]()
-    
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        setCellsView()
-        setWeekView()
-    }
-    
-    func setCellsView()
-    {
-        let width = (collectionView.frame.size.width - 2) / 8
-        let height = (collectionView.frame.size.height - 2)
-        
-        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        flowLayout.itemSize = CGSize(width: width, height: height)
-    }
-    
-    func setWeekView()
-    {
-        totalSquares.removeAll()
-        
-        var current = CalendarHelper().sundayForDate(date: selectedDate)
-        let nextSunday = CalendarHelper().addDays(date: current, days: 7)
-        
-        while (current < nextSunday)
-        {
-            totalSquares.append(current)
-            current = CalendarHelper().addDays(date: current, days: 1)
-        }
-        
-        monthLabel.text = CalendarHelper().monthString(date: selectedDate)
-            + " " + CalendarHelper().yearString(date: selectedDate)
-        collectionView.reloadData()
-        tableView.reloadData()
-    }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        totalSquares.count
+        // Do any additional setup after loading the view.
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
-        
-        let date = totalSquares[indexPath.item]
-        cell.dayOfMonth.text = String(CalendarHelper().dayOfMonth(date: date))
-        
-        if(date == selectedDate)
-        {
-            cell.backgroundColor = UIColor.systemGreen
-        }
-        else
-        {
-            cell.backgroundColor = UIColor.white
-        }
-        
-        return cell
+    @IBOutlet weak var minutesWorked: UITextView!
+    
+    @IBOutlet weak var focusLevel: UITextView!
+    
+    @IBOutlet weak var suggestedBreakTime: UITextView!
+    
+    
+    @IBAction func caluculateButtonPressed(_ sender: Any) {
+        suggestedBreakTime.text = focusLevel.text
     }
     
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
-    {
-        selectedDate = totalSquares[indexPath.item]
-        collectionView.reloadData()
-        tableView.reloadData()
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    @IBAction func previousWeek(_ sender: Any)
-    {
-        selectedDate = CalendarHelper().addDays(date: selectedDate, days: -7)
-        setWeekView()
-    }
-    
-    @IBAction func nextWeek(_ sender: Any)
-    {
-        selectedDate = CalendarHelper().addDays(date: selectedDate, days: 7)
-        setWeekView()
-    }
-    
-    override open var shouldAutorotate: Bool
-    {
-        return false
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return Event().eventsForDate(date: selectedDate).count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID") as! EventCell
-        let event = Event().eventsForDate(date: selectedDate)[indexPath.row]
-        cell.eventLabel.text = event.taskEvent + ", " + event.name + ", " + CalendarHelper().timeString(date: event.date)
-        return cell
-    }
-    
-    override func viewDidAppear(_ animated: Bool)
-    {
-        super.viewDidAppear(animated)
-        tableView.reloadData()
-    }
+    */
+
 }
-*/
